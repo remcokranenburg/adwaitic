f9eb5fa8ccfa4db87a6d3ee0c788e47f8f93a21e
Remco Kranenburg <remco@burgsoft.nl>
2023-09-26 02:07:32 +0200

    Add a bunch of standard CSS color values, plus gnome color variables

--- gnome2webcss.py
+++ gnome2webcss.py
@@ -4,42 +4,84 @@ import re
 import sys
 from pyparsing import *
 
-# This script depends on the following packages:
-#
-# - pyparsing
+color_name = Word(alphanums + "-_")
 
-# TODO: use a real parser generator like pyparsing
+color_name.run_tests("""
+    hi
+    hello_world
+    foo-bar
+""")
 
-color_name = Word("[a-z0-9_]")
-color_value = (
+color_value_variable = original_text_for(Literal("var(--") + color_name + ")")
 
+color_value_variable.run_tests("""
+    var(--hello-world)
+    var(--foo_bar)
+    var(--hi2)
+""")
+
+color_value_variable_gnome = Literal("@") + color_name("name")
+
+@color_value_variable_gnome.set_parse_action
+def convert_gnome_variable(results: ParseResults):
+    return "var(--" + results["name"] + ")"
+
+color_value_hex = Combine("#" + Word(srange("[0-9a-fA-F]")))
+
+color_channel = pyparsing_common.number ^ pyparsing_common.number + "%"
+
+color_value_rgb = original_text_for(
+    (Literal("rgb(") | Literal("rgba("))
+    + color_channel + ","
+    + color_channel + ","
+    + color_channel
+    + Opt("," + color_channel)
+    + ")"
 )
+
+
+color_value_rgb.run_tests("""
+    rgb(1, 2, 3)
+    rgba( 123 ,234, 345, 0.1)
+    rgb(100%, 1.5, -3, .1)
+""")
+
+color_value_keyword = Word(alphanums + "-")
+
+color_value = (
+    color_value_variable
+    | color_value_variable_gnome
+    | color_value_hex
+    | color_value_rgb
+    | color_value_keyword
+)
+
 color_definition = (
     Keyword("@define-color")
     + color_name("color_name")
     + color_value("color_value")
+    + ";"
 )
 
-COLOR_DEFINITION_REGEX = re.compile(r"@define-color ([a-z0-9_]*) (.*);")
-COLOR_USE_REGEX = re.compile(r"@([a-z0-9_]*)")
-CSS_MIX_REGEX = re.compile(r"(?<!color-)mix\(")
+color_definition.run_tests("""
+    @define-color foo var(--bar);
+    @define-color hello_world @foo;
+    @define-color hi2 red;
+""")
 
-def replace_color_use(matchobj):
-    return f"var(--{matchobj[1]})"
-
-def replace_all(t):
-    t = COLOR_USE_REGEX.sub(replace_color_use, t)
-    t = CSS_MIX_REGEX.sub("color-mix(", t)
-    return t
+autoname_elements()
 
 for filename in sys.argv[1:]:
     with open(filename) as f:
         for line in f:
-            color_definition = COLOR_DEFINITION_REGEX.match(line)
+            try:
+                cd = color_definition.parse_string(line)
 
-            if color_definition:
-                color_name = color_definition[1]
-                color_value = replace_all(color_definition[1])
-                print(f":root {{ --{color_name}: {color_value}; }}")
-            else:
-                print(replace_all(line))
+                # print("DEBUG:", repr(cd))
+
+                color_name = cd["color_name"]
+                color_value = cd["color_value"]
+                # print(f":root {{ --{color_name}: {color_value[0]}; }}")
+            except:
+                pass
+                print(line)
